95772dd6d891ab9b4c4b44032a81783a4e27b37b
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-10-28 13:40:01 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,78 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  let Left = 0;
+  let Top = 0;
+
+  const keyPress = (event) => {
+    let ballPositioncopy = { ...ballPosition };
+    if (event.key === "ArrowRight") {
+      Left = Left + 5;
+      ballPositioncopy.left = `${Left}px`;
+      ballPositioncopy.top = `0px`;
+      setBallPosition(ballPositioncopy);
+      console.log("move right");
+    } else if (event.key === "ArrowLeft") {
+      Left = Left - 5;
+      ballPositioncopy.left = `${Left}px`;
+      ballPositioncopy.top = `0px`;
+      setBallPosition(ballPositioncopy);
+    } else if (event.key === "ArrowDown") {
+      Top = Top + 5;
+      ballPositioncopy.left = `0px`;
+      ballPositioncopy.top = `${Top}px`;
+      setBallPosition(ballPositioncopy);
+    } else if (event.key === "ArrowUp") {
+      Top = Top - 5;
+      ballPositioncopy.left = `0px`;
+      ballPositioncopy.top = `${Top}px`;
+      setBallPosition(ballPositioncopy);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+  }, []);
+
+  const reset = () => {
+    Left = 0;
+    Top = 0;
+    let ballPositioncopy = { ...ballPosition };
+    ballPositioncopy.left = `${Left}px`;
+    ballPositioncopy.top = `${Top}px`;
+    setBallPosition(ballPositioncopy);
+  };
+  const handelClick = () => {
+    let renderball = true;
+    setRenderBall(renderball);
+  };
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else {
+      return (
+        <button className="start" onClick={handelClick}>
+          Start
+        </button>
+      );
+    }
+  };
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
       </button>
-      {renderChoice()}
+      <div className="render"> {renderChoice()}</div>
     </div>
   );
 };
